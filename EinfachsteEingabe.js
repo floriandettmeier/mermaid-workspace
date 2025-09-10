5fad5f35c9483e4000817952a9f179ef74a82de8
floriandettmeier <florian.dettmeier@gmail.com>
2025-09-10 14:20:13 +0200

    Create EinfachsteEingabe.js

---
+++ EinfachsteEingabe.js
@@ -0,0 +1,10 @@
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+     Scanner in = new Scanner(System.in);
+     String zeichenkette;
+     zeichenkette = in.nextLine();
+     System.out.println ("Zeichenkette war:"+ zeichenkette);
+     System.out.println("Programmende EinfachsteEigabe.");
+    }
+}
