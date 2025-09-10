995a36eebf5d959bf334f7b9d42b16099f28f1d6
floriandettmeier <florian.dettmeier@gmail.com>
2025-09-10 14:21:45 +0200

    Create Rechnen.js

---
+++ Rechnen.js
@@ -0,0 +1,10 @@
+class Main {
+    public static void main(String[] args) {
+     int a,b,c;
+     a=5;b=3;
+     c = a/b;
+     System.out.println(c);
+     c = a % b;
+     System.out.println(c);
+    }
+}
