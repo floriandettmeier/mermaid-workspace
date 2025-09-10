23007ebcbf359574b5ae1feba00095baa1508619
floriandettmeier <florian.dettmeier@gmail.com>
2025-09-10 13:52:39 +0200

    Update Schule.js

--- Schule.js
+++ Schule.js
@@ -1,1 +1,14 @@
-
+class Main {
+    public static void main(String[] args) {
+        int a;
+        a=5;
+        System.out.println(a++);
+        System.out.println(a);
+        System.out.println(++a);
+        System.out.println(a);
+        System.out.println(a--);
+        System.out.println(a);
+        System.out.println(--a);
+        System.out.println(a);
+    }
+}
